aa54b3d28ed485f42d9fef4b22d5f502636a6bcb
Vagish <admin@vagish.com>
2020-05-25 12:40:57 -0400

    Update branding to BGSU ACM Student Chapter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BGSU ACM Student Chapter`,
+    description: `The ACM chapter for BGSU.`,
+    author: `@BGSU-ACM`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 
 const SecondPage = (props: PageProps) => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2 ({props.path})</p>
+    <SEO title="Our Board" />
+    <h1>Our Board</h1>
+    <p>Coming soon!</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
